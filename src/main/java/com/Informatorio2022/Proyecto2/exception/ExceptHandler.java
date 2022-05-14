e89fdefefb8879f06cdb038a6678e6e7182cdcaa
Andres <rodrigueza.federacion@gmail.com>
2022-05-14 19:22:54 -0300

    improving error messages

--- src/main/java/com/Informatorio2022/Proyecto2/exception/ExceptHandler.java
+++ src/main/java/com/Informatorio2022/Proyecto2/exception/ExceptHandler.java
@@ -28,8 +28,7 @@ public class ExceptHandler {
     }
     @ExceptionHandler({NumberFormatException.class})
     public ResponseEntity<MessageInfo> numberFormatException(HttpServletRequest request) {
-        String message = messageResum.message("message.error.id.not.number", null);
-        return new ResponseEntity<>(new MessageInfo(message, HttpStatus.BAD_REQUEST.value(), request.getRequestURI()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new MessageInfo(messageResum.message("message.error.id.not.number", null), HttpStatus.BAD_REQUEST.value(), request.getRequestURI()), HttpStatus.BAD_REQUEST);
     }
     @ExceptionHandler({ConstraintViolationException.class})
     public ResponseEntity<MessagesInfo> handleValidationError(HttpServletRequest request, ConstraintViolationException exception) {
@@ -42,12 +41,10 @@ public class ExceptHandler {
     }
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class})
     public ResponseEntity<MessageInfo> httpRequestMethodNotSupportedException(HttpServletRequest request) {
-        String message = messageResum.message("user.method.not.access", null);
-        return new ResponseEntity<>(new MessageInfo(message, HttpStatus.FORBIDDEN.value(), request.getRequestURI()), HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(new MessageInfo(messageResum.message("user.method.not.access", null), HttpStatus.FORBIDDEN.value(), request.getRequestURI()), HttpStatus.FORBIDDEN);
     }
     @ExceptionHandler({HttpMessageNotReadableException.class})
     public ResponseEntity<MessageInfo> httpMessageNotReadableException(HttpServletRequest request) {
-        String message = messageResum.message("param.content.error", null);
-        return new ResponseEntity<>(new MessageInfo(message, HttpStatus.BAD_REQUEST.value(), request.getRequestURI()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(new MessageInfo(messageResum.message("param.content.error", null), HttpStatus.BAD_REQUEST.value(), request.getRequestURI()), HttpStatus.BAD_REQUEST);
     }
 }
